b2559c12d669bbe57cae35205e9073c3e5414c5b
Theara Seng <me@thearaseng.com>
2023-10-09 08:35:21 -0500

    initial commit

---
+++ script.sh
@@ -0,0 +1,7 @@
+sudo apt-get update -y
+sudo apt-get install openjdk-11-jdk -y
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo systemctl enable jenkins
